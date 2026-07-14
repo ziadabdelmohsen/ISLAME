53d17d61dcfb53fe452dabc82c1206cb9ec87cc6
Ziad Abdelmohsen <Ziad Abdelmohsen>
2026-07-14 17:19:06 +0300

    //

--- lib/widgets/active_nav_bar_icon.dart
+++ lib/widgets/active_nav_bar_icon.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+// ignore: must_be_immutable
 class ActiveNavBarIcon extends StatelessWidget {
   String iconName;
   ActiveNavBarIcon({super.key, required this.iconName});
